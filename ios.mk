2d13e5e699be90276ddc88f30dd04fa873ad2eba
Cédric Deltheil <cedric@moodstocks.com>
2013-12-11 18:21:05 +0100

    ios.mk: factorize common CFLAGS and use -Os

--- ios.mk
+++ ios.mk
@@ -11,6 +11,8 @@ IOS_SIM_CC=$(shell xcrun -sdk iphonesimulator$(IOS_SDK) -find clang)
 IOS_LIPO=$(shell xcrun -sdk iphoneos$(IOS_SDK) -find lipo)
 IOS_AR=$(shell xcrun -sdk iphoneos$(IOS_SDK) -find ar)
 
+MYCFLAGS=-I. -Ibuild -Os
+
 all: lib
 
 lib: build/liblua.a build/libfakeredis.a
@@ -26,31 +28,31 @@ build/libfakeredis.a: build/fkredis-armv7.o build/fkredis-armv7s.o \
 
 build/fkredis-armv7.o: CC=$(IOS_CC)
 build/fkredis-armv7.o: CFLAGS=-isysroot $(IOS_SDK_ROOT) -arch armv7 -mthumb \
-	-miphoneos-version-min=4.0 -I. -Ibuild
+	-miphoneos-version-min=4.0 $(MYCFLAGS)
 build/fkredis-armv7.o: fkredis.c fkredis.h fklua.h
 	$(CC) -c $(CFLAGS) $< -o $@
 
 build/fkredis-armv7s.o: CC=$(IOS_CC)
 build/fkredis-armv7s.o: CFLAGS=-isysroot $(IOS_SDK_ROOT) -arch armv7s -mthumb \
-	-miphoneos-version-min=4.0 -I. -Ibuild
+	-miphoneos-version-min=4.0 $(MYCFLAGS)
 build/fkredis-armv7s.o: fkredis.c fkredis.h fklua.h
 	$(CC) -c $(CFLAGS) $< -o $@
 
 build/fkredis-arm64.o: CC=$(IOS_CC)
 build/fkredis-arm64.o: CFLAGS=-isysroot $(IOS_SDK_ROOT) -arch arm64 \
-	-miphoneos-version-min=7.0 -I. -Ibuild
+	-miphoneos-version-min=7.0 $(MYCFLAGS)
 build/fkredis-arm64.o: fkredis.c fkredis.h fklua.h
 	$(CC) -c $(CFLAGS) $< -o $@
 
 build/fkredis-i386.o: CC=$(IOS_CC)
 build/fkredis-i386.o: CFLAGS=-isysroot $(IOS_SIM_SDK_ROOT) -arch i386 \
-	-miphoneos-version-min=4.0 -I. -Ibuild
+	-miphoneos-version-min=4.0 $(MYCFLAGS)
 build/fkredis-i386.o: fkredis.c fkredis.h fklua.h
 	$(CC) -c $(CFLAGS) $< -o $@
 
 build/fkredis-x86_64.o: CC=$(IOS_CC)
 build/fkredis-x86_64.o: CFLAGS=-isysroot $(IOS_SIM_SDK_ROOT) -arch x86_64 \
-	-miphoneos-version-min=4.0 -I. -Ibuild
+	-miphoneos-version-min=4.0 $(MYCFLAGS)
 build/fkredis-x86_64.o: fkredis.c fkredis.h fklua.h
 	$(CC) -c $(CFLAGS) $< -o $@
 
