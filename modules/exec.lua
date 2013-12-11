7206278fe1feaa37512541eb9ba9a23fcb9271d8
Cédric Deltheil <cedric@moodstocks.com>
2013-12-11 16:40:44 +0100

    modules: get rid of `table.remove`

--- modules/exec.lua
+++ modules/exec.lua
@@ -1,5 +1,5 @@
 _exec = function(rds, cmd)
   local args = _tokenize(cmd)
-  local cmd = string.lower(table.remove(args, 1))
-  return rds[cmd] and _fmt(rds[cmd](rds, unpack(args)))
+  local cmd = string.lower(args[1])
+  return rds[cmd] and _fmt(rds[cmd](rds, unpack(args, 2)))
 end
